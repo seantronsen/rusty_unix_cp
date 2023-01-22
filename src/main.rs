4cc3b8ddffd76c3afbf73f3784fefe0ef33298eb
Sean Tronsen <sean.tronsen@gmail.com>
2023-01-22 12:34:53 -0500

    [main] -- Changed project name to prepare for pushing to remote repository

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@
  * just doing this to get better with Rust.
  *
  */
-use rust_copy;
+use rusty_unix_cp;
 use std::{env, io, process};
 
 fn main() {
-    let config = rust_copy::Config::build(env::args().collect()).unwrap_or_else(|error| {
-        eprintln!("Usage: rust_copy source_a [source_b, ...] target");
+    let config = rusty_unix_cp::Config::build(env::args().collect()).unwrap_or_else(|error| {
+        eprintln!("Usage: rusty_unix_cp source_a [source_b, ...] target");
         eprintln!("config build error: {}", error);
         eprintln!("env args: {:?}", env::args());
         process::exit(1);
@@ -23,7 +23,7 @@ fn main() {
     });
 }
 
-fn run(mut config: rust_copy::Config) -> io::Result<()> {
+fn run(mut config: rusty_unix_cp::Config) -> io::Result<()> {
     let mut sources = config
         .sources
         .take()
@@ -31,13 +31,13 @@ fn run(mut config: rust_copy::Config) -> io::Result<()> {
         .into_iter();
     let target = config.target.take().expect("target was `None`");
     match config.target_isdir {
-        true => rust_copy::recursive_copy(sources, target),
+        true => rusty_unix_cp::recursive_copy(sources, target),
         false => {
             let source_file = match sources.next().expect("expected filename") {
                 Ok(val) => val,
                 Err(e) => panic!("shouldn't be able to reach here: {:?}", e.to_string()),
             };
-            rust_copy::copy(source_file, target)
+            rusty_unix_cp::copy(source_file, target)
         }
     }
 }
